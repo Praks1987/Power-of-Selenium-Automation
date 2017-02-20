20b86be62a9cd9a5077777d9e610a784b4de6743
kalim Riaz <kalimriaz@users.noreply.github.com>
2017-02-20 11:48:15 +0500

    How to read data from excel in selenium webdriver using java
    How to read data from excel in selenium webdriver using java.

--- read_excel_data.java
+++ read_excel_data.java
@@ -1,16 +1,12 @@
 package sampletest3;
-
 import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Date;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.write.*;
@@ -20,7 +16,6 @@ import jxl.write.Number;
 
 public class read_excel_data {
 	
-	
   private WebDriver driver;
   //private String baseUrl;
   
@@ -29,26 +24,17 @@ public class read_excel_data {
   @Before
   public void setUp() throws Exception {    
 	  
-	 //driver = new FirefoxDriver();
-    // baseUrl = "https://www.google.com.pk/";
-    // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    
-    //String FilePath = "D://pms_login.xls";
     File exlFile = new File("D:/pms_login_read.xls");
     FileInputStream fs = new FileInputStream(exlFile);
 
     Workbook wb = Workbook.getWorkbook(fs);
     Sheet sh = wb.getSheet(0);
     
-    /*String CellGetContent = sh.getCell(0,0).getContents();
-    System.out.println(CellGetContent);
-    // GetCell(Column,Row);
-    
     String CellGetContent01 = sh.getCell(1,0).getContents();
     System.out.println(CellGetContent01);
     
     String CellGetContent02 = sh.getCell(0,1).getContents();
-    System.out.println(CellGetContent02); */
+    System.out.println(CellGetContent02);
     
     WritableWorkbook writableWorkbook = Workbook.createWorkbook(exlFile);
     WritableSheet writableSheet = writableWorkbook.createSheet( "pms_login", 0);
@@ -58,25 +44,13 @@ public class read_excel_data {
     Boolean bool = new Boolean(2, 0, true);
     Number num = new Number(3, 0, 9.99);
 
-   // Add the created Cells to the sheet
+    // Add the created Cells to the sheet
     writableSheet.addCell(label);
     writableSheet.addCell(date);
     writableSheet.addCell(bool);
     writableSheet.addCell(num);
   }
-  
-  
 
-  @Test
-  public void testLogin() throws Exception {
-    //driver.get(baseUrl + "/");
-    //driver.findElement(By.id("lst-ib")).clear();
-   // driver.findElement(By.id("lst-ib")).sendKeys("mtbc.com");
-   // driver.findElement(By.name("btnG")).click();
-    //driver.findElement(By.linkText("MTBC: Medical Billing Services – Medical Billing Company")).click();
-  }
-
-  @After
   public void tearDown() throws Exception {
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
@@ -84,8 +58,4 @@ public class read_excel_data {
       fail(verificationErrorString);
     }
   }
-
-  
 }
-
-
